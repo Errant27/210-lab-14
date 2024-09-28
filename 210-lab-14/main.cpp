862528b54c6656cae9eed656b2de887c61c5bcf6
Errant27 <kneil.sgo@gmail.com>
2024-09-27 21:44:19 -0700

    commit 1

--- 210-lab-14/main.cpp
+++ 210-lab-14/main.cpp
@@ -1,14 +1,59 @@
-//
-//  main.cpp
-//  210-lab-14
-//
-//  Created by Neil Orton on 9/27/24.
-//
-
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+class Color
+{
+private:
+    int red;
+    int blue;
+    int green;
+    
+public:
+    void setRed(int r) {
+        red = r;
+    }
+    
+    void setBlue(int b) {
+        blue = b;
+    }
+    
+    void setGreen(int g) {
+        green = g;
+    }
+    
+    int getRed() {
+        return red;
+    }
+    
+    int getBlue() {
+        return blue;
+    }
+    
+    int setGreen() {
+        return green;
+    }
+    
+    void print_colors() {
+        cout << "R: " << red << endl;
+        cout << "B: " << blue << endl;
+        cout << "G: " << green << endl;
+    }
+    
+}; // End of class
+
+
+
+int main()
+{
+    Color colorOne;
+    colorOne.setRed(12);
+    colorOne.setBlue(52);
+    colorOne.setGreen(39);
+    
+    colorOne.print_colors();
+    
+    
     return 0;
 }
